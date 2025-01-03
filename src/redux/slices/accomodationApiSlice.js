b484a1cf2fb389be32d6eb31b03761bc944d3a14
Kamal Nayan Upadhyay <knupadhyay784@gmail.com>
2025-01-03 11:26:25 +0530

    dine

--- src/redux/slices/accomodationApiSlice.js
+++ src/redux/slices/accomodationApiSlice.js
@@ -10,7 +10,6 @@ export const accomodationApi = apiSlice.injectEndpoints({
                 method: 'POST',
                 body: accomodationData,
                 formData: true,
-                credentials : true
             }),
             onQueryStarted: async (arg, { dispatch, queryFulfilled }) => {
                 // Optimistic cache update: patch the shop list before the mutation completes
